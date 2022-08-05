2103496f866540a46e239f4d06b2dfd56e140119
Chaitanya Gupta <mail@chaitanyagupta.com>
2022-08-05 13:41:54 +0530

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+CFLAGS = -Iinclude -Lbuild
+
+build/hello-world: src/hello-world.c build/libglad.dylib
+	cc $(CFLAGS) `pkg-config --cflags --libs glfw3` -lglad -o $@ $<
+
+build/libglad.dylib: src/glad.c
+	cc -c $(CFLAGS) -o $@ $<
+
+.PHONY: run
+run: build/hello-world
+	$<	
---
+++ src/hello-world.c
@@ -0,0 +1,164 @@
+// currently reading: https://learnopengl.com/Getting-started/Hello-Triangle
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
+}
+
+// info log - for storing error messages, etc.
+char infoLog[512];
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+
+const char *fragmentShaderSource = "#version 330 core\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}\n";
+
+int makeShader(GLenum shaderType, const char *source, unsigned int *shader) {
+  *shader = glCreateShader(shaderType);  
+  glShaderSource(*shader, 1, &source, NULL);
+  glCompileShader(*shader);
+  int success;
+  glGetShaderiv(*shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(*shader, sizeof(infoLog), NULL, infoLog);
+    return 0;
+  }
+  return success;
+}
+
+int makeShaderProgram(unsigned int vertexShader, unsigned int fragmentShader, unsigned int *program) {
+  *program = glCreateProgram();
+  glAttachShader(*program, vertexShader);
+  glAttachShader(*program, fragmentShader);
+  glLinkProgram(*program);
+  int success;
+  glGetProgramiv(*program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(*program, sizeof(infoLog), NULL, infoLog);
+    return 0;
+  }
+  return success;
+}
+
+int main()
+{
+  // glfw initialization
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+    {
+      printf("Failed to create GLFW window\n");
+      glfwTerminate();
+      return -1;
+    }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad initialization
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+      printf("Failed to initialize GLAD\n");
+      return -1;
+    }    
+  //glViewport(0, 0, 800, 600);
+
+  unsigned int vertexShader;
+  if (!makeShader(GL_VERTEX_SHADER, vertexShaderSource, &vertexShader)) {
+    printf("Vertex shader could not be made: %s\n", infoLog);
+    return 1;
+  }
+
+  unsigned int fragmentShader;
+  if (!makeShader(GL_FRAGMENT_SHADER, fragmentShaderSource, &fragmentShader)) {
+    printf("Fragment shader could not be made: %s\n", infoLog);
+    return 1;
+  }
+
+  unsigned int shaderProgram;
+  if (!makeShaderProgram(vertexShader, fragmentShader, &shaderProgram)) {
+    printf("Shader program could not be made: %s\n", infoLog);
+    return 1;
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+  };
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+  glBindVertexArray(0); 
+
+  // The event loop
+  while(!glfwWindowShouldClose(window))
+    {
+      // input
+      processInput(window);
+
+      // rendering
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);      
+
+      glUseProgram(shaderProgram);
+      glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+
+      // call events
+      glfwSwapBuffers(window);
+
+      // swap buffers
+      glfwPollEvents();    
+    }
+
+  // Finish
+  glfwTerminate();  
+
+  return 0;
+}
+
